f8004be4bcd8b9c0e8b804d9fbde83912c64e41f
thebinhdx <157784623+Thebinhdx@users.noreply.github.com>
2025-11-02 15:50:23 +0700

    Add files via upload

---
+++ paneltools.ps1
@@ -0,0 +1,25 @@
+# launcher.ps1
+# ------------------------------------------
+# PowerShell bootstrapper
+# ------------------------------------------
+
+$batUrl = "https://raw.githubusercontent.com/yourname/YourRepo/main/run.bat"
+
+$tempBat = "$env:TEMP\runme.bat"
+
+Write-Host "Installing File" -ForegroundColor Cyan
+Invoke-WebRequest -Uri $batUrl -OutFile $tempBat -UseBasicParsing
+
+if (!(Test-Path $tempBat)) {
+    Write-Host "Error: Can't Install File, Code:404" -ForegroundColor Red
+    exit 1
+}
+
+# Chạy file batch
+Write-Host "Starting File" -ForegroundColor Yellow
+Start-Process "cmd.exe" -ArgumentList "/c", "`"$tempBat`"" -Wait
+
+# Xoá file tạm sau khi chạy
+Remove-Item $tempBat -Force
+
+Write-Host "Succesfully" -ForegroundColor Green
